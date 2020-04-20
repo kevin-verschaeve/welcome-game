b3174dded74823dd836af5afa498931be8e7bb03
Kevin Verschaeve <kevin.verschaeve@live.fr>
2020-04-21 01:02:40 +0200

    add plans

--- src/Game.svelte
+++ src/Game.svelte
@@ -11,14 +11,30 @@
 
         gameRef.set(game);
     };
+
+    const objectiveDone = (game, gameRef, plan) => {
+        const p  = game.plans.find(p => p.level == plan.level);
+        p.done = !p.done;
+        gameRef.set(game);
+    };
 </script>
 
 <Doc path={`games/${params.game}`} let:data={game} let:ref={gameRef}>
-    <CardRow cards={game.firstRow}/>
+    <div id="plans">
+        {#each game.plans as plan}
+            <a href="#" on:click|preventDefault={objectiveDone(game, gameRef, plan)} style="display: inline-block; ">
+                <img src="plans/level{plan.level}/{plan.done ? 'complete' : 'initial'}/{plan.objective}.png" alt="{plan.level}">
+            </a>
+        {/each}
+    </div>
 
-    <CardRow cards={game.secondRow} let:card>
-        <img src="actions/{card.action}.png" alt="{card.action}" style="max-width: 100%;">
-    </CardRow>
+    <div id="actions">
+        <CardRow cards={game.firstRow}/>
+
+        <CardRow cards={game.secondRow} let:card>
+            <img src="actions/{card.action}.png" alt="{card.action}" style="max-width: 100%;">
+        </CardRow>
+    </div>
 
     <button on:click={nextRound(game, gameRef)}>Suivant</button>
 </Doc>
--- src/Services/GameManager.js
+++ src/Services/GameManager.js
@@ -4,10 +4,16 @@ export function newGame() {
   const deck = getDeck();
   const firstRow = deck.splice(deck.length - 3, deck.length);
   const secondRow = deck.splice(deck.length - 3, deck.length);
+  const plans = [
+    {objective: Math.floor(Math.random() * 5) + 1, done: false, level: 1},
+    {objective: Math.floor(Math.random() * 5) + 1, done: false, level: 2},
+    {objective: Math.floor(Math.random() * 5) + 1, done: false, level: 3},
+  ];
 
   return {
     deck,
     firstRow,
     secondRow,
+    plans,
   };
 }
