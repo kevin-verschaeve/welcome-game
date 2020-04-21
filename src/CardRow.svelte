b21ad9265844b68bea39a22813aaf9bcf7a2188d
Clément Petitcunot <clement.petitcunot@gmail.com>
2020-04-21 20:19:09 +0200

    Integration

--- src/CardRow.svelte
+++ src/CardRow.svelte
@@ -4,7 +4,7 @@
 
 <div class="card-row" style="display: flex;">
     {#each cards as card}
-        <div style="padding: 20px; border: 1px solid black; width: 200px;">
+        <div style="padding: 20px; width: 200px;">
             <slot {card}>{card.value}</slot>
         </div>
     {/each}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import App from './App.svelte';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 
 var app = new App({
 	target: document.body
